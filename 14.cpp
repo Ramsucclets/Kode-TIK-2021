c2084373fab219ab64a5f3d746314b499ed9a97c
Ramsucclets <68151701+Ramsucclets@users.noreply.github.com>
2021-05-05 13:54:10 +0700

    ARRAY HELL

--- 14.cpp
+++ 14.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
+#include <numeric> 
 using namespace std;
 int main()
 {
-    string nama;
-    int NT; 
+    int NT,i; 
+    int Bilangan[4];
+    cout<<"Masukan Bilangan 1 - 4 "<<endl; 
+     for (int i = 1; i < 5; i++) {
+        cout << "Bilangan " << i << " : "; 
+        cin >> Bilangan[i];
+
+    }
+    cout << "Bilangan 1-4 :  ";
+    for (int n = 1; n < 5; ++n) {
+        cout << Bilangan[n] << "  ";
+    }
+    int n = 4, sum = 0;
+    sum = accumulate(Bilangan, Bilangan+n, sum);
+    cout<<"\nHasil : "<<sum;
+    NT=sum/4;
+    cout<<"\nRata-Rata ： "<< NT ;
     
-
-    
-
-
-
-
 }
     
 
